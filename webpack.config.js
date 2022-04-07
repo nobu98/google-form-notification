4e80b4b71222220a90e56d3d8d0b6e6ae41b5078
田村泰伸 <yasunobu.tamura@karadanote.jp>
2022-04-07 16:53:28 +0900

    remove babel

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,12 @@ const CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
   mode: "development",
   devtool: "inline-source-map",
-  context: __dirname,
   entry: {
     main: path.resolve(__dirname, "src", "index.ts"),
   },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "index.js",
+    filename: "bundle.js",
   },
   resolve: {
     extensions: [".ts", ".js"],
@@ -22,9 +21,9 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.[tj]s$/,
+        test: /\.tsx?$/,
+        use: "ts-loader",
         exclude: /node_modules/,
-        loader: "babel-loader",
       },
     ],
   },
